64db9d6ef21ccc6984af641bb5f5e125002ea548
Tim Fetter <tim.fetter@magicjack.com>
2020-04-08 15:04:28 -0400

    implemented firebase utils, included ability to store authenticated users into our firestorm database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyDm5k5O6EAMmSNJoZXp8vGvt389-oqkeR0",
@@ -10,7 +10,33 @@ const config = {
   storageBucket: "crwn-db-41bed.appspot.com",
   messagingSenderId: "411917513981",
   appId: "1:411917513981:web:6a58d59f8788b15732ba94",
-  measurementId: "G-6V595RFVPP"
+  measurementId: "G-6V595RFVPP",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("there was an error creating the user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
@@ -19,7 +45,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
